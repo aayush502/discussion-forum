3a1d92de395fbb46e936d5680aa9690d92abc4ef
Aayush <humagain.aayush83@gmail.com>
2021-06-06 12:43:59 +0545

    jhbds

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Discussions
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.asset.compile = false 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
