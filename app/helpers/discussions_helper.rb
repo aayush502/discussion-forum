91a209092f6cebe4090c299b3656fffeafa4d6fd
Aayush <humagain.aayush83@gmail.com>
2021-06-24 21:46:22 +0545

    hdf

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -4,6 +4,8 @@ module DiscussionsHelper
     user_signed_in? && current_user.id == discussion.user_id
   end
 
-  
+  def reply_author(reply)
+    user_signed_in? && current_user.id == reply.user_id
+  end
 
 end
